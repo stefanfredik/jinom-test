757df99b700cf470f70665f861b73f84058a2c37
stefanfredik <stefanfredik@users.noreply.github.com>
2026-04-15 16:23:18 +0800

    Update binaries and assembly information for FoTestingApp

--- FoTestingApp/obj/Debug/net8.0-windows/FoTestingApp.AssemblyInfo.cs
+++ FoTestingApp/obj/Debug/net8.0-windows/FoTestingApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Aplikasi Pengujian dan Sertifikasi Jaringan Fiber Optik")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b613572dfd775a59a35c60800dc293ac2dd6589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f1f9051f1e6bbba5be644ac883efb5a1013263")]
 [assembly: System.Reflection.AssemblyProductAttribute("FO Testing & Commissioning")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoTestingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
